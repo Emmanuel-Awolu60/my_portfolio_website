920a3126eff58220c143ff3bad4b6dce50177ce4
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-07-17 20:14:20 +0100

    Set up React + Tailwind with Vite inside main repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 bg-black text-white text-4xl font-bold">
+    Tailwind Works!
+  </div>
+  
+  )
+}
+
+export default App
